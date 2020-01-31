7e29be8e7aa9ff497fa48b925bfc925332dc302b
Chris Elten <chris.elten@mindbridge.ai>
2020-01-30 22:13:41 -0500

    add hc to stock component

--- src/app/stock/stock.component.ts
+++ src/app/stock/stock.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { IexService } from '../iex.service';
 import { of } from 'rxjs';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-stock',
@@ -10,10 +11,14 @@ import { of } from 'rxjs';
   styleUrls: ['./stock.component.scss']
 })
 export class StockComponent implements OnInit {
-  // todo - create a class
+  // todo - create classes as necessary
   stockSym: string;
   stockData;
   companyData;
+
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options;
+  updateFlag = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -26,10 +31,31 @@ export class StockComponent implements OnInit {
       switchMap((params: ParamMap) => {
         const sym = params.get('symbol');
         this.stockSym = sym;
-        this.service.getStock(sym).subscribe(res => this.stockData = res);
+        this.service.getStock(sym).subscribe((res: []) => {
+          this.stockData = res;
+          const series = [];
+          series.push({ name: 'High', type: 'line', data: res.map(el => el.high)} );
+          series.push({ name: 'Low', type: 'line', data: res.map(el => el.low)} );
+          const categories = res.map(el => el.date);
+          this.chartOptions = this.getChartOptions(sym, series, categories);
+          this.updateFlag = true;
+        });
         this.service.getCompany(sym).subscribe(res => this.companyData = res);
         return of(true);
       }))
       .subscribe();
+  }
+
+  getChartOptions(title, series, categories) {
+    return {
+      title,
+      yAxis: {
+        title: {
+            text: 'Price'
+        }
+      },
+      series,
+      xAxis: {categories}
+    };
   }
 }
